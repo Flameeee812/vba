645c754fe92d5c032b0d0d3011a69b2917f212e6
Michael <Misha2006060906@gmail.com>
2024-12-25 13:16:15 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,27 +1,3 @@
-import sqlite3 as sql
 from .db_utils import *
-from .config import DATABASE
 
-
-def get_connection():
-    connection = sql.connect(DATABASE, check_same_thread=False)
-    cursor = connection.cursor()
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Taxpayers (
-        id INTEGER PRIMARY KEY,
-        initials TEXT NOT NULL,
-        electricity INTEGER,
-        cold_water INTEGER,
-        hot_water INTEGER,
-        gas INTEGER,
-        debt REAL,
-        payment REAL
-        )
-        ''')
-    connection.commit()
-    return connection
-
-
-def close(connection):
-    connection.close()
+connection = get_connection()
