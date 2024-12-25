533e317c00cb03403a6c6e4580160ab349ea1c4a
Michael <Misha2006060906@gmail.com>
2024-12-25 17:34:26 +0300

    Delete app/scheduler.py

--- app/scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from database import connection, update_debts
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(lambda: update_debts(connection), 'cron', day=15, hour=0)
-scheduler.start()
