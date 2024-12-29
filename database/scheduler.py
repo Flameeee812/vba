88bd90474f283cdd935c43bb980ae69933173365
Michael <Misha2006060906@gmail.com>
2024-12-29 10:40:11 +0400

    Update scheduler.py

--- database/scheduler.py
+++ database/scheduler.py
@@ -1,13 +1,38 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .db_utils import update_debts
+from .db_utils import reset_to_zero_debt, reset_to_zero_readings
 from logger import app_logger
 
 
-def start_task(connection):
-    """Функция для старта фоновой задачи по обновлению долгов"""
+def schedule_reset_debt(connection):
+    reset_to_zero_debt(connection)
+
+
+def schedule_reset_readings(connection):
+    reset_to_zero_readings(connection)
+
+
+def run_background_tasks(connection):
+    """
+    Функция для старта фоновых задач по обновлению долгов и сбросу показаний счётчиков.
+
+    Параметры:
+    1. connection - подключение к базе данных
+"""
 
     scheduler = BackgroundScheduler()
-    scheduler.add_job(lambda: update_debts(connection), 'cron', day=15, hour=0)
+
+    try:
+        scheduler.add_job(schedule_reset_debt, 'cron', day=15, hour=0, args=[connection])
+        app_logger.info("Задача reset_to_zero_debt успешно добавлена.")
+    except Exception as e:
+        app_logger.error(f"Ошибка при добавлении задачи reset_to_zero_debt: {e}")
+
+    try:
+        scheduler.add_job(schedule_reset_readings, 'cron', day=15, hour=0, args=[connection])
+        app_logger.info("Задача reset_to_zero_readings успешно добавлена.")
+    except Exception as e:
+        app_logger.error(f"Ошибка при добавлении задачи reset_to_zero_readings: {e}")
+
     scheduler.start()
     app_logger.info("Планировщик задач запущен")
 
@@ -21,5 +46,3 @@ def end_task(scheduler):
     app_logger.info("Планировщик задач остановлен")
 
     return None
-
-
